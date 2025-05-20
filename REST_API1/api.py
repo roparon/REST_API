3b8592cf6b4ae483e725ff67ec5b27942082eeac
Aron Rop <aaronrop40@gmail.com>
2025-05-20 11:34:14 +0300

    created a project about restful api

--- REST_API1/api.py
+++ REST_API1/api.py
@@ -1,13 +1,50 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, marshal_with, fields, reqparse, abort
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///api.db'
+db = SQLAlchemy(app)
+api = Api(app)
 
 
-@app.route('/')
-def home():
-    return '<h1>Welcome to RESTful API</h1>'
+
+
+#Database model
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db. Column(db.String(80), unique=True, nullable=False)
+    email = db. Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"{self.username} {self.email}"
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('username', type=str, required=True, help = "Username cannot be blank!")
+user_args.add_argument('email', type=str, required=True, help = "email cannot be blank!")
+user_fields = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'email': fields.String
+}
+
+class User(Resource):
+    @marshal_with(user_fields)
+    def get(self):
+        users = UserModel.query.all
+        if not users:
+            abort (404, message ='Users not found')
+        return users
+    
+    
+
+api.add_resource(User, '/api/users')
+
+
+
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
