94c52dc98451b7348bc59ef278e83d39b646cbdf
Aron Rop <aaronrop40@gmail.com>
2025-05-22 16:02:00 +0300

    added enrollment.py with several files

---
+++ School_Management_System/app/models/enrollment.py
@@ -0,0 +1,16 @@
+from app.extensions import db
+from datetime import datetime, timezone
+
+
+class EnrollmentModel(db.Model):
+    __tablename__ = "enrollments"
+    id = db.Column(db.Integer, primary_key=True)
+    students_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    enrolment_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    grade = db.Column(db.String(2))
+    status = db.Column(db.String(20), default='enrolled') # enrolled, completed,dropped
+
+
+    def __repr__(self):
+        return f"Enrollment: {self.id}"
