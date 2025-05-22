6a6e01bfdb01abdc2a5aa0947f63e2fd91bfa05c
Aron Rop <aaronrop40@gmail.com>
2025-05-22 15:59:20 +0300

    added __init__.py with several files

---
+++ School_Management_System/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.students import StudentModel
+from app.models.teacher import TeacherModel
+from app.models.course import CourseModel
+from app.models.enrollment import EnrollmentModel
+from app.models.users import UserModel
+from app.models.fee import FeeModel
