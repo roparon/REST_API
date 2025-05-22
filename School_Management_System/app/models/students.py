36304b1473ac542eb2771522c6dbccb6e2958ea0
Aron Rop <aaronrop40@gmail.com>
2025-05-22 16:03:52 +0300

    added students.py with several files

---
+++ School_Management_System/app/models/students.py
@@ -0,0 +1,20 @@
+from app.extensions import db
+from datetime import datetime, timezone
+from app.models.enrollment import EnrollmentModel
+from app.models.fee import FeeModel
+
+class StudentModel(db.Model):
+    __tablename__ ='students'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    students_id = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_of_birth = db.Column(db.Date)
+    enrollment_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    enrollment = db.relationship('EnrollmentModel', backref='student', lazy=True)
+    fees = db.relationship('FeeModel', backref='student_ref', lazy=True)
+
+
+    def __repr__(self):
+        return f"{self.student_id} {self.first_name} {self.last_name}"
