e620fec5e9aab2098481d89a756e521022e58324
Aron Rop <aaronrop40@gmail.com>
2025-05-23 14:12:39 +0300

    created an ecoursepy resource

--- School_Management_System/app/__init__.py
+++ School_Management_System/app/__init__.py
@@ -5,7 +5,7 @@ from app.resources.user import Users, User
 from app.resources.teacher import Teachers, Teacher
 from app.resources.student import Students, Student
 from app.resources.enrollment import Enrollments
-from app.resources.course import Courses as course
+from app.resources.course import Courses as course, Courses as courses
 from app.extensions import db
 from config import Config
 from flask_migrate import Migrate
@@ -32,6 +32,6 @@ api.add_resource(Students, '/api/students')
 api.add_resource(Student, '/api/students/<int:id>')
 api.add_resource(Enrollments, '/api/enrollments')
 api.add_resource(course, '/api/courses')
-api.add_resource(course, '/api/course/<int:id>')
+api.add_resource(courses, '/api/courses/<int:id>')
 
 
