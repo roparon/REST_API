29c58744ac168092869478e733eb4e2228e9dfdf
Aron Rop <aaronrop40@gmail.com>
2025-05-27 15:17:13 +0300

    modified my innitial file

--- School_Management_System/app/__init__.py
+++ School_Management_System/app/__init__.py
@@ -20,13 +20,13 @@ Swagger_config = {
         {
             "endpoint": 'apispec_1',
             "route": '/apispec_1.json',
-            "rule_filter": lambda rule: True,  # all in
-            "model_filter": lambda tag: True,  # all in
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
         }
     ],
     "static_url_path": "/flasgger_static",
     "swagger_ui": True,
-    "specs_route": "/swagger/"
+    "specs_route": "/apidocs/"
 }
 
 
